4839354617716616b4efdb04830fa688e8d35bce
Rick Gladwin <rick@iddesign.ca>
2022-05-03 18:22:29 -0400

    add config/cli_characters file and sheet_presenter test file

---
+++ lib/src/config/cli_characters.dart
@@ -0,0 +1,3 @@
+// define cli characters for use in the presenters
+
+const top_left_border = '';
---
+++ test/lib/src/presentation/presenters/sheet_presenter_test.dart
@@ -0,0 +1,14 @@
+// unit tests etc. for sheet presenter
+
+import 'package:test/test.dart';
+
+// known good default sheet presenter output
+// TODO: build using characters in src/config
+
+void main() {
+  group('SheetPresenter', () => {
+    group('CLI', () {
+      test('presents a default sheet', () {}, skip: 'TODO: build default presenter, match using known good');
+    })
+  });
+}
