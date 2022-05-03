61984a2701c93e6c2b461667e84d9bf0bc17b8e1
Rick Gladwin <rick@iddesign.ca>
2022-05-02 21:55:15 -0400

    add Sheet basic test todos

---
+++ test/lib/src/domain/entities/sheet_test.dart
@@ -0,0 +1,12 @@
+// unit tests for sheet entities
+
+import 'package:dart_sudoku/src/domain/entities/sheet.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Sheet', () {
+    test('initializes with a grid of 9x9 nodes', () {}, skip: 'TODO: initialize a Sheet');
+    test('initializes with arguments', () {}, skip: 'TODO: initialize a Sheet of SheetNodes from a dataset');
+    test('is indexable by row and column to the SheetNode level', () {}, skip: 'TODO: ensure indexable Sheet data');
+  });
+}
