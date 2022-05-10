5a8b65b47e0452a1cdc781f792af442e2adbbd9a
Rick Gladwin <rick@iddesign.ca>
2022-05-10 15:33:33 -0400

    update readme, git description, and main_test

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # Dart Sudoku
 
-## A simple command-line Dart application for solving Sudoku puzzles.
+A simple command-line Dart application for solving Sudoku puzzles.
 
+## Test
+
+### run all tests:
+- line by line:
+`dart test --reporter=expanded`
+- single line:
+`dart test --reporter=compact`
+
+### run test(s) based on name:
+`dart test --name sheet [--name main]`
+
+### run single test based on filename:
+`dart test/lib/src/domain/entities/sheet_test.dart`
---
+++ test/lib/src/main_test.dart
@@ -0,0 +1,17 @@
+// acceptance tests etc. for main
+
+import 'package:test/test.dart';
+
+void main() {
+  group('Main', () => {
+    group('Solves easy puzzles', () {
+      test('solves easy puzzle', () {}, skip: 'TODO: fulfill acceptance criteria for easy puzzle');
+    }),
+    group('Solves medium puzzles', () {
+      test('solves medium puzzle', () {}, skip: 'TODO: fulfill acceptance criteria for medium puzzle');
+    }),
+    group('Solves hard puzzles', () {
+      test('solves hard puzzle', () {}, skip: 'TODO: fulfill acceptance criteria for hard puzzle');
+    })
+  });
+}
