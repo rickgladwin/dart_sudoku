4d85bd4936e6c69ff32e381814de598de9857715
Rick Gladwin <rick@iddesign.ca>
2022-06-10 16:11:05 -0400

    sheet_solver finds all solved SheetNodes

--- lib/src/domain/usecases/sheet_solver.dart
+++ lib/src/domain/usecases/sheet_solver.dart
@@ -1,2 +1,22 @@
 // the main logic for solving sudoku puzzles
 
+import 'package:dart_sudoku/src/domain/entities/sheet.dart';
+import 'package:dart_sudoku/src/domain/entities/sheet_node.dart';
+import 'package:dart_sudoku/src/domain/usecases/sheet_node_handler.dart';
+
+class SheetSolver {
+  late Sheet sheet;
+  Set<SheetNode> solvedNodes = {};
+
+  SheetSolver(this.sheet);
+
+  void findSolvedNodes() {
+    for (var row in sheet.rows) {
+      for (var sheetNode in row) {
+        if (SheetNodeHandler(sheetNode).isSolved()) {
+          solvedNodes.add(sheetNode);
+        }
+      }
+    }
+  }
+}
--- test/lib/src/domain/usecases/sheet_solver_test.dart
+++ test/lib/src/domain/usecases/sheet_solver_test.dart
@@ -1,16 +1,71 @@
+import 'package:dart_sudoku/src/domain/entities/sheet.dart';
+import 'package:dart_sudoku/src/domain/entities/sheet_node.dart';
+import 'package:dart_sudoku/src/domain/usecases/sheet_solver.dart';
 import 'package:test/test.dart';
 
 
 main() {
-  group('Eliminate Solutions', () {
+  group('Eliminate Solutions:', () {
 
     test('finds a solved node', () {
+      // create solved SheetNodes
+      // init each SheetNode with a unique set of integers of length 1
+      List<List<SheetNode>> sheetNodeData = [[],[],[],[],[],[],[],[],[]];
 
-    }, skip: 'TODO: build solved node finder');
+      final solvedNode = SheetNode({3});
+
+      for (var i = 1; i <= 9; i++) {
+        for (var j = 1; j <= 9; j++) {
+          if (i == 5 && j == 3) {
+            // add a solved SheetNode
+            sheetNodeData[i-1].add(solvedNode);
+          } else {
+            // add a default SheetNode
+            sheetNodeData[i-1].add(SheetNode());
+          }
+        }
+      }
+
+      var sheetInitializer = SheetInitializer(rowData: sheetNodeData);
+      var sheet = Sheet(sheetInitializer);
+
+      var sheetSolver = SheetSolver(sheet);
+      sheetSolver.findSolvedNodes();
+
+      expect(sheetSolver.solvedNodes, {solvedNode});
+    });
 
     test('finds all solved nodes', () {
+      // create solved SheetNodes
+      // init each SheetNode with a unique set of integers of length 1
+      List<List<SheetNode>> sheetNodeData = [[],[],[],[],[],[],[],[],[]];
 
-    }, skip: 'find/track all solved nodes');
+      final solvedNode1 = SheetNode({3});
+      final solvedNode2 = SheetNode({3});
+
+      for (var i = 1; i <= 9; i++) {
+        for (var j = 1; j <= 9; j++) {
+          if (i == 5 && j == 3) {
+            // add a solved SheetNode
+            sheetNodeData[i-1].add(solvedNode1);
+          } else if (i == 7 && j == 4) {
+            // add another solved SheetNode
+            sheetNodeData[i-1].add(solvedNode2);
+          } else {
+            // add a default SheetNode
+            sheetNodeData[i-1].add(SheetNode());
+          }
+        }
+      }
+
+      var sheetInitializer = SheetInitializer(rowData: sheetNodeData);
+      var sheet = Sheet(sheetInitializer);
+
+      var sheetSolver = SheetSolver(sheet);
+      sheetSolver.findSolvedNodes();
+
+      expect(sheetSolver.solvedNodes, {solvedNode1, solvedNode2});
+    });
 
     test('eliminates solutions across a row', () {
 
@@ -25,7 +80,7 @@ main() {
     }, skip: 'TODO: seek and remove solutions in the same sector as a solved node');
   });
 
-  group('Evaluate Sheet', () {
+  group('Evaluate Sheet:', () {
     test('stops if the sheet is solved', () {
 
     }, skip: 'TODO: stop if the sheet is solved (a higher abstract than checking in the handler)');
@@ -34,4 +89,10 @@ main() {
 
     }, skip: 'TODO: create halting logic/conditions');
   });
+}
+
+class Stub {
+  /*
+
+   */
 }
