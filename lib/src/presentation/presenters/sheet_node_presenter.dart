3cff42fe3ece7022616e3de9688d75150009157c
Rick Gladwin <rick@iddesign.ca>
2022-06-10 12:00:21 -0400

    presents solved SheetNode

--- lib/src/presentation/presenters/sheet_node_presenter.dart
+++ lib/src/presentation/presenters/sheet_node_presenter.dart
@@ -1,6 +1,14 @@
 import 'dart:io';
 import 'package:dart_sudoku/src/domain/entities/sheet_node.dart';
 import 'package:ansi_escapes/ansi_escapes.dart';
+import 'package:dart_sudoku/src/domain/usecases/sheet_node_handler.dart';
+
+class Chars {
+  // static const removed = '•';
+  static const removed = ' ';
+  static const canvasBlank = '#';
+  static const nodeBlank = ' ';
+}
 
 
 class SheetNodePresenter {
@@ -12,13 +20,54 @@ class SheetNodePresenter {
 
   void writeSheetNode({required SheetNode sheetNode, required int x, required int y}) {
     canvas.write(ansiEscapes.curserTo(x, y));
+
+    if (SheetNodeHandler(sheetNode).isSolved()) {
+      writeSolvedSheetNode(sheetNode: sheetNode, x: x, y: y);
+    } else {
+      writeUnsolvedSheetNode(sheetNode: sheetNode, x: x, y: y);
+    }
+
+    // int nodeRow = y;
+    // for (var i = 1; i <= 9; i++) {
+    //   // sleep(Duration(milliseconds:1));
+    //   if (sheetNode.solutions.contains(i)) {
+    //     canvas.write(i);
+    //   } else {
+    //     canvas.write(Chars.removed);
+    //   }
+    //   if (i % 3 == 0) {
+    //     ++nodeRow;
+    //     canvas.write(ansiEscapes.curserTo(x, nodeRow));
+    //   }
+    // }
+  }
+
+  void writeUnsolvedSheetNode({required SheetNode sheetNode, required int x, required int y}) {
     int nodeRow = y;
     for (var i = 1; i <= 9; i++) {
       // sleep(Duration(milliseconds:1));
       if (sheetNode.solutions.contains(i)) {
         canvas.write(i);
       } else {
-        canvas.write('•');
+        canvas.write(Chars.removed);
+      }
+      if (i % 3 == 0) {
+        ++nodeRow;
+        canvas.write(ansiEscapes.curserTo(x, nodeRow));
+      }
+    }
+  }
+
+  void writeSolvedSheetNode({required SheetNode sheetNode, required int x, required int y}) {
+    int nodeRow = y;
+    int solution = sheetNode.solutions.first;
+
+    for (var i = 1; i <= 9; i++) {
+      // sleep(Duration(milliseconds:1));
+      if (i == 5) {
+        canvas.write(solution);
+      } else {
+        canvas.write(Chars.nodeBlank);
       }
       if (i % 3 == 0) {
         ++nodeRow;
@@ -33,7 +82,7 @@ class SheetNodePresenter {
     for(var i = 1; i <= (3 * yNodes); i++) {
       // cols
       for (var j = 1; j <= (3 * xNodes); j++) {
-        canvas.write('#');
+        canvas.write(Chars.canvasBlank);
       }
       canvas.write('\n');
     }
@@ -49,25 +98,14 @@ void printCoords(x, y) {
 }
 
 void main() {
-  var sheetNode = SheetNode();
+  // var sheetNode = SheetNode();
+  // var sheetNode = SheetNode({3,6,7});
+  var sheetNode = SheetNode({8});
   var sheetNodePresenter = SheetNodePresenter();
-  sheetNodePresenter.printBlankSheetNodes(xNodes: 1, yNodes: 1);
+  // sheetNodePresenter.printBlankSheetNodes(xNodes: 1, yNodes: 1);
   sheetNodePresenter.writeSheetNode(sheetNode: sheetNode, x: 0, y: 0);
+
   sheetNodePresenter.printCanvas();
   print('\ncodeUnits:\n');
   print(sheetNodePresenter.canvas.toString().codeUnits);
-  // sleep(Duration(seconds: 1));
-  // print('\n***\n');
-  // stdout.writeln(sheetNodePresenter.canvas.toString().runes);
-  // log(sheetNodePresenter.canvas.toString());
-  // print(sheetNodePresenter.canvas.toString());
-  // print('\n***\n');
-
-  // sheetNodePresenter.printBlankSheetNodes(xNodes: 9, yNodes: 9);
-  // for (var i = 0; i < 9*3; i+=3) {
-  //   for (var j = 0; j < 9*3; j+=3) {
-  //     sheetNodePresenter.writeSheetNode(sheetNode: sheetNode, x: j, y: i);
-  //     sheetNodePresenter.printCanvas();
-  //   }
-  // }
 }
--- test/lib/src/presentation/presenters/sheet_node_presenter_test.dart
+++ test/lib/src/presentation/presenters/sheet_node_presenter_test.dart
@@ -1,6 +1,3 @@
-import 'dart:io';
-
-import 'package:ansi_escapes/ansi_escapes.dart';
 import 'package:dart_sudoku/src/domain/entities/sheet_node.dart';
 import 'package:dart_sudoku/src/presentation/presenters/sheet_node_presenter.dart';
 import 'package:test/test.dart';
@@ -8,17 +5,9 @@ import 'package:test/test.dart';
 import '../../../../test_utilities/comparisons.dart';
 
 
-class KnownGood {
-  // 123
-  // 456
-  // 789
-  static const defaultSheetNode = [27, 91, 49, 59, 49, 72, 49, 50, 51, 27, 91, 50, 59, 49, 72, 52, 53, 54, 27, 91, 51, 59, 49, 72, 55, 56, 57, 27, 91, 52, 59, 49, 72];
-}
-
 void main() {
-  group('SheetNodePresenter', () {
-    test('presents a default SheetNode', () async {
-      // stdout.write(ansiEscapes.clearScreen);
+  group('SheetNodePresenter:', () {
+    test('presents a default SheetNode', () {
       final sheetNode = SheetNode();
       final canvas = StringBuffer();
       final knownGoodSheetNode = KnownGood.defaultSheetNode;
@@ -26,23 +15,60 @@ void main() {
 
       sheetNodePresenter.writeSheetNode(sheetNode: sheetNode, x: 0, y: 0);
 
-      // print('\n***\n');
-      // stdout.writeln(sheetNodePresenter.canvas.toString().codeUnits);
-      // print('\n***\n');
-      // print(knownGoodSheetNode.toString().runes);
       expect(bufferEqualsCodeUnits(sheetNodePresenter.canvas, knownGoodSheetNode), true);
-      // expect(sheetNodePresenter.canvas, equals(knownGoodSheetNode));
-
-
-      // stdout.write('\n\n\n\n');
-      // sleep(Duration(seconds: 2));
-      // read canvas
-      // stdout.write(ansiEscapes.curserTo(0, 0));
-      // var checkChar = await stdin.elementAt(2);
-      // stdout.write('\n');
-      // print(checkChar);
-      // print('okay');
     });
-    // });
+    test('presents a partially solved SheetNode', () {
+      final sheetNode = SheetNode({3,6,7});
+      final canvas = StringBuffer();
+      final knownGoodSheetNode = KnownGood.threeSixSevenSheetNode;
+      var sheetNodePresenter = SheetNodePresenter(canvas);
+
+      sheetNodePresenter.writeSheetNode(sheetNode: sheetNode, x: 0, y: 0);
+
+      // print('\n\n\n');
+      // sheetNodePresenter.printCanvas();
+      // print('\n\n\n');
+      // print(sheetNodePresenter.canvas.toString().codeUnits);
+
+      expect(bufferEqualsCodeUnits(sheetNodePresenter.canvas, knownGoodSheetNode), true);
+    });
+    test('presents a solved SheetNode', () {
+      final sheetNode = SheetNode({8});
+      final canvas = StringBuffer();
+      final knownGoodSheetNode = KnownGood.eightSheetNode;
+      var sheetNodePresenter = SheetNodePresenter(canvas);
+
+      sheetNodePresenter.writeSheetNode(sheetNode: sheetNode, x: 0, y: 0);
+
+      // print('\n\n\n');
+      // sheetNodePresenter.printCanvas();
+      // print('\n\n\n');
+      // print(sheetNodePresenter.canvas.toString().codeUnits);
+
+      expect(bufferEqualsCodeUnits(sheetNodePresenter.canvas, knownGoodSheetNode), true);
+    });
   });
+}
+
+class KnownGood {
+  /*
+  123
+  456
+  789
+  */
+  static const defaultSheetNode = [27, 91, 49, 59, 49, 72, 49, 50, 51, 27, 91, 50, 59, 49, 72, 52, 53, 54, 27, 91, 51, 59, 49, 72, 55, 56, 57, 27, 91, 52, 59, 49, 72];
+
+  /*
+    3
+    6
+  7
+   */
+  static const threeSixSevenSheetNode = [27, 91, 49, 59, 49, 72, 32, 32, 51, 27, 91, 50, 59, 49, 72, 32, 32, 54, 27, 91, 51, 59, 49, 72, 55, 32, 32, 27, 91, 52, 59, 49, 72];
+
+  /*
+
+   8
+
+  */
+  static const eightSheetNode = [27, 91, 49, 59, 49, 72, 32, 32, 32, 27, 91, 50, 59, 49, 72, 32, 56, 32, 27, 91, 51, 59, 49, 72, 32, 32, 32, 27, 91, 52, 59, 49, 72];
 }
