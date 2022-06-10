8cf389a2cc6f1a90ef79035d676e8f8997105146
Rick Gladwin <rick@iddesign.ca>
2022-06-10 17:27:45 -0400

    refactor test to generate test sheet in one place

--- test/lib/src/domain/usecases/sheet_solver_test.dart
+++ test/lib/src/domain/usecases/sheet_solver_test.dart
@@ -73,29 +73,12 @@ main() {
 
     test('eliminates solutions across a row', () {
       // create solved SheetNodes
-      // init each SheetNode with a unique set of integers of length 1
-      List<List<SheetNode>> sheetNodeData = [[],[],[],[],[],[],[],[],[]];
-
       final solvedSet = {3};
       final defaultMinusSolved = defaultSolutions.difference(solvedSet);
-      final solvedNode = SheetNode(solvedSet);
       final solvedNodeX = 4;
       final solvedNodeY = 6;
 
-      for (var i = 1; i <= 9; i++) {
-        for (var j = 1; j <= 9; j++) {
-          if (i == solvedNodeY && j == solvedNodeX) {
-            // add a solved SheetNode
-            sheetNodeData[i - 1].add(solvedNode);
-          } else {
-            // add a default SheetNode
-            sheetNodeData[i-1].add(SheetNode());
-          }
-        }
-      }
-
-      var sheetInitializer = SheetInitializer(rowData: sheetNodeData);
-      var sheet = Sheet(sheetInitializer);
+      var sheet = createDummySheet(solvedSet, solvedNodeX, solvedNodeY);
 
       var sheetSolver = SheetSolver(sheet);
 
@@ -105,7 +88,7 @@ main() {
       sheetPresenter.writeSheet(sheet);
       sheetPresenter.printCanvas();
 
-      sleep(Duration(seconds: 3));
+      sleep(Duration(seconds: 1));
 
       for (var i = 0; i < 9; i++) {
         if (i != solvedNodeX - 1) {
@@ -116,29 +99,12 @@ main() {
 
     test('eliminate solutions in a column', () {
       // create solved SheetNodes
-      // init each SheetNode with a unique set of integers of length 1
-      List<List<SheetNode>> sheetNodeData = [[],[],[],[],[],[],[],[],[]];
-
       final solvedSet = {3};
       final defaultMinusSolved = defaultSolutions.difference(solvedSet);
-      final solvedNode = SheetNode(solvedSet);
       final solvedNodeX = 4;
       final solvedNodeY = 6;
 
-      for (var i = 1; i <= 9; i++) {
-        for (var j = 1; j <= 9; j++) {
-          if (i == solvedNodeY && j == solvedNodeX) {
-            // add a solved SheetNode
-            sheetNodeData[i - 1].add(solvedNode);
-          } else {
-            // add a default SheetNode
-            sheetNodeData[i-1].add(SheetNode());
-          }
-        }
-      }
-
-      var sheetInitializer = SheetInitializer(rowData: sheetNodeData);
-      var sheet = Sheet(sheetInitializer);
+      var sheet = createDummySheet(solvedSet, solvedNodeX, solvedNodeY);
 
       var sheetSolver = SheetSolver(sheet);
 
@@ -175,6 +141,33 @@ main() {
 
     }, skip: 'TODO: create halting logic/conditions');
   });
+}
+
+Sheet createDummySheet([Set<int>? solvedSetArg, int? solvedSetXArg, int? solvedSetYArg]) {
+  // create solved SheetNodes
+  // init each SheetNode with a unique set of integers of length 1
+  List<List<SheetNode>> sheetNodeData = [[],[],[],[],[],[],[],[],[]];
+
+  final solvedSet = solvedSetArg ?? {};
+  final defaultMinusSolved = defaultSolutions.difference(solvedSet);
+  final solvedNode = (solvedSetArg != null) ? SheetNode(solvedSetArg) : SheetNode();
+  final solvedNodeX = solvedSetXArg;
+  final solvedNodeY = solvedSetYArg;
+
+  for (var i = 1; i <= 9; i++) {
+    for (var j = 1; j <= 9; j++) {
+      if (i == solvedNodeY && j == solvedNodeX) {
+        // add a solved SheetNode
+        sheetNodeData[i - 1].add(solvedNode);
+      } else {
+        // add a default SheetNode
+        sheetNodeData[i-1].add(SheetNode());
+      }
+    }
+  }
+
+  var sheetInitializer = SheetInitializer(rowData: sheetNodeData);
+  return Sheet(sheetInitializer);
 }
 
 class Stub {
